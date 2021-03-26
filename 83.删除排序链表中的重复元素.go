d3483a61cfe46421beba17fe58c7b5d5238b232d
loyo <1963597184@qq.com>
2021-03-26 23:18:25 +0800

    链表重复删除

---
+++ 83.删除排序链表中的重复元素.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=golang
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+        return head 
+    }
+
+	head.Next  = deleteDuplicates(head.Next) 
+	if head.Val == head.Next.Val {//结构体循环调用                    
+        head = head.Next                  
+    }
+	return head
+}
+// @lc code=end
+
